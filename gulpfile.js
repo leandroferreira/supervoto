91c0f952dabcde0b5deb9f297f5cdabde13c2ac3
Leandro Ferreira <durang@gmail.com>
2014-09-08 09:12:18 -0300

    integrado compass ao gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,13 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var imagemin = require('gulp-imagemin');
 var sourcemaps = require('gulp-sourcemaps');
+var compass = require('gulp-compass');
 var del = require('del');
 
 var paths = {
   scripts: ['src/js/**/*.js'],
-  images: 'src/img/**/*'
+  images: 'src/img/*[png,jpg,jpeg,gif,ico]',
+  sass: 'src/sass/**/*.sass'
 };
 
 // clean build folder
@@ -47,10 +49,28 @@ gulp.task('images', ['clean'], function() {
     .pipe(gulp.dest('build/img'));
 });
 
+// copy misc static files to build folder
+gulp.task('statics', ['clean'], function(cb) {
+  gulp.src('src/*.html').pipe(gulp.dest('build'));
+  gulp.src('src/fonts/**/*').pipe(gulp.dest('build/fonts'));
+  cb();
+});
+
+// compile compass
+gulp.task('compass', function() {
+  gulp.src(paths.sass)
+  .pipe(compass({
+    config_file: './config.rb',
+    sass: 'src/sass',
+    css: 'build/css'
+  }));
+});
+
 // watch for changes
 gulp.task('watch', function() {
   gulp.watch(paths.scripts, ['scripts']);
   gulp.watch(paths.images, ['images']);
+  gulp.watch(paths.sass, ['compass']);
 });
 
 /////////////////
@@ -61,7 +81,7 @@ gulp.task('watch', function() {
 gulp.task('install', ['bower-install']);
 
 // do a single build
-gulp.task('build', ['images', 'scripts', 'bower-copy']);
+gulp.task('build', ['compass', 'images', 'statics', 'scripts', 'bower-copy']);
 
 // defaults to dev mode
 gulp.task('default', ['build', 'watch']);
