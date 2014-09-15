919eeb672cada50e7c6003cae0d87a65f1ea3cc0
Leandro Ferreira <durang@gmail.com>
2014-09-15 03:30:42 -0300

    pequenas correcoes no isotope

--- build/js/supervoto/home.js
+++ build/js/supervoto/home.js
@@ -3,43 +3,45 @@ define(['jquery', 'isotope', 'mustache', 'supervoto/home-menu'], function ($, Is
     var thisObj = this;
 
     var _politicoTemplate = '' +
-      '<li class="card {{estado}} {{partido}} {{cargo}}">' +
-      '    <div class="front">' +
-      '        <header>' +
-      '            <div class="thumb"><img src="{{imagem}}" alt="{{nome}}" /></div>' +
-      '            <h3>{{nome}} <span class="partido">{{partido}}</span></h3>' +
-      '            <h4>{{cargo}} / {{estado}}</h4>' +
-      '        </header>' +
-      '        <ul class="badges">' +
-      '            <li class="badge-badge1"></li>' +
-      '        </ul>' +
-      '    </div>' +
-      '    <div class="back">' +
-      '        <header>' +
-      '            <h3>{{nome}} <span class="partido">{{partido}}</span></h3>' +
-      '        </header>' +
-      '        <ul class="badges">' +
-      '            <li class="badge-badge1"></li>' +
-      '        </ul>' +
-      '        <ul class="features">' +
-      '            <li>' +
-      '                <h4>atuação</h4>' +
-      '                <span class="bar"></span><span class="value">{{atributos.atuacao}}</span>' +
-      '            </li>' +
-      '            <li>' +
-      '                <h4>processos</h4>' +
-      '                <span class="bar"></span><span class="value">{{atributos.processos}}</span>' +
-      '            </li>' +
-      '            <li>' +
-      '                <h4>privilégios</h4>' +
-      '                <span class="bar"></span><span class="value">{{atributos.privilegios}}</span>' +
-      '            </li>' +
-      '            <li>' +
-      '                <h4>assiduidade</h4>' +
-      '                <span class="bar"></span><span class="value">{{atributos.assiduidade}}</span>' +
-      '            </li>' +
-      '        </ul>' +
-      '    </div>' +
+      '<li class="isotope-item {{estado}} {{partido}} {{cargo}}">' +
+      '  <div class="card">' +
+      '      <div class="front">' +
+      '          <header>' +
+      '              <div class="thumb"><img src="{{imagem}}" alt="{{nome}}" /></div>' +
+      '              <h3>{{nome}} <span class="partido">{{partido}}</span></h3>' +
+      '              <h4>{{cargo}} / {{estado}}</h4>' +
+      '          </header>' +
+      '          <ul class="badges">' +
+      '              <li class="badge-badge1"></li>' +
+      '          </ul>' +
+      '      </div>' +
+      '      <div class="back">' +
+      '          <header>' +
+      '              <h3>{{nome}} <span class="partido">{{partido}}</span></h3>' +
+      '          </header>' +
+      '          <ul class="badges">' +
+      '              <li class="badge-badge1"></li>' +
+      '          </ul>' +
+      '          <ul class="features">' +
+      '              <li>' +
+      '                  <h4>atuação</h4>' +
+      '                  <span class="bar"></span><span class="value">{{atributos.atuacao}}</span>' +
+      '              </li>' +
+      '              <li>' +
+      '                  <h4>processos</h4>' +
+      '                  <span class="bar"></span><span class="value">{{atributos.processos}}</span>' +
+      '              </li>' +
+      '              <li>' +
+      '                  <h4>privilégios</h4>' +
+      '                  <span class="bar"></span><span class="value">{{atributos.privilegios}}</span>' +
+      '              </li>' +
+      '              <li>' +
+      '                  <h4>assiduidade</h4>' +
+      '                  <span class="bar"></span><span class="value">{{atributos.assiduidade}}</span>' +
+      '              </li>' +
+      '          </ul>' +
+      '      </div>' +
+      '  </div>' +
       '</li>';
     var _politicosURL = 'js/politicos.json'
     var _isotope;
@@ -92,7 +94,7 @@ define(['jquery', 'isotope', 'mustache', 'supervoto/home-menu'], function ($, Is
 
     var _startIsotope = function() {
       _isotope = new Isotope('.isotope', {
-        itemSelector: '.card',
+        itemSelector: '.isotope-item',
         transitionDuration: 0.2,
         layoutMode: 'masonry',
         masonry: {
